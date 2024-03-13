217a383f5cd6aeee92ccbba9f8be75f5812ce920
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-03-13 22:22:34 +0900

    install package for vercel

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,13 @@
 import db from '@astrojs/db';
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		db(),
-		tailwind({
-			nesting: true,
-		}),
-	],
-	output: 'server',
-	adapter: node({
-		mode: 'standalone',
-	}),
+  integrations: [db(), tailwind({
+    nesting: true
+  })],
+  output: 'server',
+  adapter: vercel()
 });
